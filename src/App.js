f3bcb12a19abde17e7de48ccd94865e11b48e851
Simon Busch <simon.busch@live.fr>
2020-12-17 15:21:57 +0100

    add formulaire

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import './App.css'
+import Formulaire from './components/Formulaire'
 
 class App extends Component {
   render () {
     return (
       <div className='box'>
-        <h2>hello world</h2>
+        <div>
+          <div className="messages">
+            <p>messages</p>
+          </div>
+          <Formulaire  />
+        </div>
       </div>
     )
   }
